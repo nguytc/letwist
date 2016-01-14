fa31b48768b5bdbedfabcce8f7ce62e74e931099
Tim Nguyen <nguy1877@umn.edu>
2016-01-13 23:49:06 -0600

    Extensive comments added. fixed shuffling

--- LeTwist.java
+++ LeTwist.java
@@ -1,3 +1,9 @@
+/*
+Tim Nguyen
+2016
+LeTwist: Anagram game
+*/
+
 import java.util.HashMap;
 import java.io.BufferedReader;
 import java.io.File;
@@ -6,15 +12,15 @@ import java.io.IOException;
 import java.util.LinkedList;
 import java.util.Random;
 import java.util.Scanner;
-import java.util.Timer;
 import java.lang.StringBuilder;
 
 public class LeTwist {
-    private HashMap<Integer, LinkedList<String>> words;
-    private int targetWordSize;
-    private int score;
-    private Round round;
-    private int rounds;
+    private HashMap<Integer, LinkedList<String>> words; // all possible words
+
+    private int targetWordSize; // maximum word length
+    private int score; // player's current score
+    private Round round; // information for the current round
+    private int rounds; // the current round number
 
     public LeTwist() {
         words = new HashMap<Integer, LinkedList<String>>();
@@ -27,7 +33,13 @@ public class LeTwist {
         rounds = 0;
     }
 
+    // PREPROCESS =============================================================
     public void preprocess(String wordsFile) {
+        /*  Obtain the words from the list of words given in the file.
+            Store them in a hashmap called words. The key will be the length of
+            the words and the value will be a linked list of the words with the
+            key as the length. */
+
         try (BufferedReader br = new BufferedReader(new FileReader(wordsFile))) {
             String line;
             while ((line = br.readLine()) != null) {
@@ -43,7 +55,12 @@ public class LeTwist {
         } catch (Exception e) {}
     }
 
+    // TARGET =================================================================
     public String target() {
+        /*  Randomly attain a word of the maximum word length to serve as the
+            target word. The target word serves as the basis for the rest of
+            the words that can be found, since its letters are used. */
+
         Random random = new Random();
 
         LinkedList<String> possible = words.get(targetWordSize);
@@ -53,9 +70,15 @@ public class LeTwist {
         return word;
     }
 
+    // FIND ANAGRAMS ==========================================================
     public HashMap<Integer, LinkedList<String>> findAnagrams(String target) {
+        /*  Slight misnomer. Find all words that can be formed from the letters
+            of the target word. */
+
         HashMap<Integer, LinkedList<String>> anagrams = new HashMap<Integer, LinkedList<String>>();
+
         for (int len : words.keySet()) {
+            // Smallest word length will be 3
             if (len > targetWordSize || len <= 2) continue;
 
             LinkedList<String> possible = words.get(len);
@@ -76,9 +99,10 @@ public class LeTwist {
         return anagrams;
     }
 
+    // CONTAINS ===============================================================
     public boolean contains(String s1, String s2) {
-        /*  See if the second string can be made from the letters in the first
-            string */
+        /*  Determine if the second string can be made from the letters in the
+            first string */
 
         HashMap<Character, Integer> letters = new HashMap<Character, Integer>();
 
@@ -90,6 +114,7 @@ public class LeTwist {
             else letters.put(focus, 1);
         }
 
+        // see if letters in second string are contained in first string
         for (int i = 0; i < s2.length(); i++) {
             char focus = s2.charAt(i);
 
@@ -103,40 +128,30 @@ public class LeTwist {
         return true;
     }
 
+    // SHUFFLE LETTERS ========================================================
     public void shuffleLetters() {
-        
+        /* Shuffle the letters that are displayed */
+        round.shuffle();
     }
 
-    // Fisher–Yates shuffle
-    public char[] shuffle(String s) {
-        char[] letters = s.toCharArray();
-
-        Random random = new Random();
-        for (int i = letters.length - 1; i > 0; i--) {
-            int index = random.nextInt(i + 1);
-            // Simple swap
-            char tmp = letters[index];
-            letters[index] = letters[i];
-            letters[i] = tmp;
-        }
-
-        return letters;
-    }
-
+    // CLEAR SCREEN ===========================================================
     public void clearScreen() {
+        /* Clear the terminal screen */
+
         System.out.print("\033[H\033[2J");
         System.out.flush();
         System.out.println("LeTwist\n\nEnter \"help me\" for help\n");
     }
 
-    public int getScore() {
-        return score;
-    }
-
+    // GUESS ==================================================================
     public boolean guess(String guess) {
+        /*  Player guesses a word that can be formed from the given letters.
+            If it can be, keep track of it and award points. */
+
         HashMap<String, Boolean> found = round.getFound();
 
         if (found.containsKey(guess) && !found.get(guess)) {
+            // points given is length of word squared times 10
             int points = guess.length() * guess.length() * 10;
             score += points;
             found.put(guess, true);
@@ -146,26 +161,36 @@ public class LeTwist {
         return false;
     }
 
+    // ROUND DISPLAY ==========================================================
     public void roundDisplay(int timeLeft, boolean end, String message) {
+        /*  Display the current state of the game to the player.
+            This includes the words that can be formed by the letters, where
+            the word is displayed if found or masked with dashes if not.
+            Show the letters, score, time left, and round also.
+            If this is the end of the round, handle it accordingly */
+
         HashMap<Integer, LinkedList<String>> anagrams = round.getAnagrams();
         HashMap<String, Boolean> found = round.getFound();
         char[] letters = round.getLetters();
 
         StringBuilder display = new StringBuilder();
 
+        // Display the possible formed words to the player
         int column = 0;
         for (int key : anagrams.keySet()) {
             LinkedList<String> possible = anagrams.get(key);
             for (String focus : possible) {
+                // display the entire word if already found or end of round
                 if (found.get(focus)) display.append(focus);
                 else if (end) display.append(focus.toUpperCase());
-                else {
+                else { // if not found, mask words with dashes that show length
                     for (int i = 0; i < focus.length(); i++) {
                         display.append("-");
                     }
                 }
                 display.append("\t");
 
+                // put 6 words in a row for more compact word board
                 column++;
                 if (column == 6) {
                     display.append("\n");
@@ -175,11 +200,13 @@ public class LeTwist {
         }
         display.append("\n\n");
 
+        // display the letters that can be used
         for (int i = 0; i < letters.length; i++) {
             display.append(letters[i] + " ");
         }
         display.append("\n\nScore: " + score);
 
+        // Display the time remaining in the round in mm:ss format
         if (end) {
             display.append("\tTime: 00:00");
         } else {
@@ -192,26 +219,42 @@ public class LeTwist {
         }
 
         display.append("\tRound: " + rounds + "\n");
+
+        // message concisely gives the player info
         display.append(message + "\n");
 
         System.out.println(display.toString());
     }
 
+    // NEW ROUND ==============================================================
     public void newRound() {
+        /* Initiate a new round with new letters and words */
+
         round = new Round(this);
         rounds++;
     }
 
+    // QUALIFY FOR NEXT ROUND =================================================
     public boolean qualifyForNextRound() {
-        return round.qualifyForNextRound();
+        /*  A player qualifies for the next round only if the player found at
+            least one of the maximum letter words. */
+
+        return round.qualifyForNextRound(targetWordSize);
     }
 
+    // CHEAT THE TARGET =======================================================
     public void cheatTheTarget() {
+        /*  NOTE: This is a hidden cheat feature
+            Automatically guess one of the largest words to pass round */
+
         String target = round.getTarget();
         guess(target);
     }
 
+    // PRINT HELP =============================================================
     public void printHelp() {
+        /* Print possible commands the user can input */
+
         String display = "\nEnter:\n" +
                         "guess - a word that can be made out of the letters\n" +
                         "\"next round\" - to go to the next round\n" +
@@ -222,7 +265,10 @@ public class LeTwist {
         System.out.println(display);
     }
 
+    // MAIN ===================================================================
     public static void main(String[] args) {
+        /* The driver should be extracted out to own class eventually */
+
         Scanner scanner = new Scanner(System.in);
         LeTwist game = new LeTwist();
 
@@ -238,13 +284,14 @@ public class LeTwist {
                 int timeLeft = 120 - (currentTime - startTime);
                 game.roundDisplay(timeLeft, false, msg);
 
-
+                boolean nextRound = false;
                 while (true) {
                     String input = scanner.nextLine().toLowerCase();
 
                     if (input.equals("next round")) {
-                        break;
+                        nextRound = true;
                     } else if (input.equals("shuffle me")) {
+                        game.shuffleLetters();
                         msg = "Shuffled";
                     } else if (input.equals("exit game")) {
                         game.clearScreen();
@@ -265,6 +312,7 @@ public class LeTwist {
                     break;
                 }
 
+                if (nextRound) break;
                 currentTime = (int)System.currentTimeMillis() / 1000;
             }
 
--- Round.java
+++ Round.java
@@ -1,24 +1,31 @@
 import java.util.LinkedList;
 import java.util.HashMap;
+import java.util.Random;
 
 public class Round {
-    private String target;
-    private char[] letters;
+    private String target; // target word
+    private char[] letters; // letters in target word shuffled
+
+    // words that can be formed with letters of the target word
     private HashMap<Integer, LinkedList<String>> anagrams;
-    private HashMap<String, Boolean> found;
+    private HashMap<String, Boolean> found; // keeps track of words found
 
     public Round(LeTwist game) {
         target = game.target();
-        letters = game.shuffle(target);
+        shuffle();
+
         anagrams = game.findAnagrams(target);
         found = new HashMap<String, Boolean>();
 
+        // key in found are the words. value is false if not found yet, or true
+        // if it has been found
         for (int key : anagrams.keySet()) {
             LinkedList<String> possible = anagrams.get(key);
             for (String focus : possible) found.put(focus, false);
         }
     }
 
+    // GETTERS ================================================================
     public String getTarget() {
         return target;
     }
@@ -35,8 +42,30 @@ public class Round {
         return found;
     }
 
-    public boolean qualifyForNextRound() {
-        LinkedList<String> needed = anagrams.get(7);
+    // SHUFFLE ================================================================
+    public void shuffle() {
+        /*  Perform the Fisher-Yates shuffle to shuffle the letters of the
+            target word to be displayed to the player. */
+        char[] shuffled = target.toCharArray();
+
+        Random random = new Random();
+        for (int i = shuffled.length - 1; i > 0; i--) {
+            int index = random.nextInt(i + 1);
+            // Simple swap
+            char tmp = shuffled[index];
+            shuffled[index] = shuffled[i];
+            shuffled[i] = tmp;
+        }
+
+        letters = shuffled;
+    }
+
+    // QUALIFY FOR NEXT ROUND =================================================
+    public boolean qualifyForNextRound(int targetWordSize) {
+        /*  A player qualifies for the next round only if the player found at
+            least one of the maximum letter words. */
+
+        LinkedList<String> needed = anagrams.get(targetWordSize);
 
         for (String word : needed) {
             if (found.get(word)) return true;
